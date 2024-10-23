d763ee61711c2299debe136be2726387d3248116
rutwiksaraf <rutwiksaraf3@gmail.com>
2024-10-23 17:44:12 -0400

    added spacy in setup

--- setup.py
+++ setup.py
@@ -3,9 +3,17 @@ from setuptools import setup, find_packages
 setup(
 	name='project1',
 	version='1.0',
-	author='You Name',
-	authour_email='your ufl email',
+	author='Rutwik Saraf',
+	author_email='rutwiksaraf@ufl.edu',
 	packages=find_packages(exclude=('tests', 'docs')),
-	setup_requires=['pytest-runner'],
-	tests_require=['pytest']	
-)
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'spacy'],
+    install_requires=[
+        'spacy'
+    ],
+    entry_points={
+        'console_scripts': [
+            'download_spacy_model=spacy:cli.download:main',
+        ]
+    }
+)	
