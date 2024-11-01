2b0dcdec3c9db19eb77d3acfe4d4d5bfbc2cd4a3
rutwiksaraf <rutwiksaraf3@gmail.com>
2024-10-31 20:54:52 -0400

    Adding images to readme, removing dummy text files

--- setup.py
+++ setup.py
@@ -1,28 +1,3 @@
-# from setuptools import setup, find_packages
-# from setuptools.command.install import install
-# import os
-
-# # Custom command to download the 'en_core_web_lg' model after installation
-# class CustomInstall(install):
-#     def run(self):
-#         install.run(self)  # Install all dependencies
-#         os.system('python -m spacy download en_core_web_md')  # Download the Spacy model
-
-# setup(
-#     name='project1',
-#     version='1.0',
-#     author='Your Name',
-#     author_email='your ufl email',
-#     packages=find_packages(exclude=('tests', 'docs')),
-#     setup_requires=['pytest-runner'],
-#     tests_require=['pytest', 'spacy'],
-#     install_requires=[
-#         'spacy'
-#     ],
-#     cmdclass={
-#         'install': CustomInstall,  # Add the custom install command
-#     }
-# )
 from setuptools import setup, find_packages
 
 setup(
